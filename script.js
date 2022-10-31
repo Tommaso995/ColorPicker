53dd34562b9bbba902cad920fc66afd14c365b66
Sarah Profeta <sarahprofeta13@gmail.com>
2022-10-31 17:58:01 +0100

    Versione funzionante! :)

--- script.js
+++ script.js
@@ -1,12 +1,45 @@
-let btn = document.querySelector(".btn");
+let pulsanteRosso = document.getElementById("rosso");
+let pulsanteBlu = document.getElementById("blu");
+let pulsanteVerde = document.getElementById("verde");
+let pulsanteGrigio = document.getElementById("grigio");
 
-btn.addEventListener("click", cambiaColore);
+pulsanteRosso.addEventListener("click", cambiaColoreRosso);
+pulsanteBlu.addEventListener("click", cambiaColoreBlu);
+pulsanteVerde.addEventListener("click", cambiaColoreVerde);
+pulsanteGrigio.addEventListener("click", cambiaColoreGrigio);
 
-function cambiaColore (e) {
+function cambiaColoreRosso (e) {
     e.preventDefault();
     console.log("Clicked!");
 
     let target = document.querySelector("#changingBg");
     console.log(target);
-    target.style.backgroundColor= btn.innerText;
+    target.style.backgroundColor= pulsanteRosso.innerText;
+};
+
+function cambiaColoreBlu (e) {
+    e.preventDefault();
+    console.log("Clicked!");
+
+    let target = document.querySelector("#changingBg");
+    console.log(target);
+    target.style.backgroundColor= pulsanteBlu.innerText;
+};
+
+function cambiaColoreVerde (e) {
+    e.preventDefault();
+    console.log("Clicked!");
+
+    let target = document.querySelector("#changingBg");
+    console.log(target);
+    target.style.backgroundColor= pulsanteVerde.innerText;
+};
+
+function cambiaColoreGrigio (e) {
+    e.preventDefault();
+    console.log("Clicked!");
+
+    let target = document.querySelector("#changingBg");
+    console.log(target);
+    target.style.backgroundColor= pulsanteGrigio.innerText;
 };
